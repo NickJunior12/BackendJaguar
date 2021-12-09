817d5192d7271187089d651fbb86868a054d0af5
delfos85 <61164365+delfos85@users.noreply.github.com>
2021-12-09 04:34:18 -0600

    Actualizamos la conexion a bd interacthink

--- db/connection.ts
+++ db/connection.ts
@@ -1,7 +1,14 @@
 import { Sequelize } from 'sequelize';
 
-const db = new Sequelize('jaguardb','root','123456', {
-    host: 'localhost',
+// const db = new Sequelize('jaguardb','root','123456', {
+//     host: 'localhost',
+//     port: 3306,
+//     dialect: 'mysql'
+
+// })
+
+const db = new Sequelize('u117119134_jaguardb','u117119134_jaguaradmin','Mexico2021', {
+    host: 'sql441.main-hosting.eu',
     port: 3306,
     dialect: 'mysql'
 
--- dist/db/connection.js
+++ dist/db/connection.js
@@ -1,8 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-const db = new sequelize_1.Sequelize('jaguardb', 'root', '123456', {
-    host: 'localhost',
+// const db = new Sequelize('jaguardb','root','123456', {
+//     host: 'localhost',
+//     port: 3306,
+//     dialect: 'mysql'
+// })
+const db = new sequelize_1.Sequelize('u117119134_jaguardb', 'u117119134_jaguaradmin', 'Mexico2021', {
+    host: 'sql441.main-hosting.eu',
     port: 3306,
     dialect: 'mysql'
 });
