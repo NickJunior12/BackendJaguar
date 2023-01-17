6d520f9e4a062d54898042ca573e7b004e2a05b6
unknown <cortes.ubaldo@outlook.com>
2023-01-16 21:31:16 -0600

    Se agrega la conexion

--- dist/db/connection.js
+++ dist/db/connection.js
@@ -1,12 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-// const db = new Sequelize('jaguardb','root','123456', {
-//     host: 'localhost',
-//     port: 3306,
-//     dialect: 'mysql'
-// })
-const db = new sequelize_1.Sequelize('u117119134_jaguardb', 'u117119134_jaguaradmin', 'Mexico2021', {
+const baseProd = 'u117119134_jaguardb';
+const usuarioProd = 'u117119134_jaguaradmin';
+const baseQA = 'u117119134_jaguardbqa';
+const usuarioQA = 'u117119134_jaguarqa';
+const db = new sequelize_1.Sequelize(baseProd, usuarioProd, 'Mexico2021', {
     host: 'sql441.main-hosting.eu',
     port: 3306,
     dialect: 'mysql'
